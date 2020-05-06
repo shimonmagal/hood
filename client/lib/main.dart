1d587d86f7c60d3f0eec28960ea0a4816ab34656
David Levanon <david.levanon@takipi.com>
2020-05-06 12:59:51 +0000

    initial client - flyer page + adding flyer page

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -55,7 +56,7 @@ class MyAppState extends State<MyApp> {
   }
   
   @override
-  Widget build(BuildContext BuildContext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -71,6 +72,19 @@ class MyAppState extends State<MyApp> {
             }
             
             return CircularProgressIndicator();
+          }
+        ),
+        floatingActionButton: Builder(
+          builder: (BuildContext context) {
+            return FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => AddFlyerForm())
+                );
+              },
+              child: Icon(Icons.add_circle),
+              backgroundColor: Colors.blue,
+            );
           }
         ),
       ),
@@ -123,11 +137,81 @@ class FlyerView extends StatelessWidget {
       appBar: AppBar(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text(flyer.title, style: Theme.of(context).textTheme.headline),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: Image.network(flyer.imageUrl, height: 300.0)
+            ),
             Text(flyer.description, style: Theme.of(context).textTheme.body1),
+          ]
+        )
+      ),
+    );
+  }
+}
+
+class AddFlyerForm extends StatefulWidget {
+  @override
+  AddFlyerFormState createState() {
+    return AddFlyerFormState();
+  }
+}
+
+class AddFlyerFormState extends State<AddFlyerForm> {
+  final _formKey = GlobalKey<FormState>();
+  String existingPhotoUrl;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Flyer')), 
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter title';
+                }
+                
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.title),
+                hintText: 'Enter the title'
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter description';
+                }
+                
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.description),
+                hintText: 'Enter description'
+              ),
+            ),
+
+            //FormField<String>(
+            //  builder:
+            //),
+            
+            new Container(
+              padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+              child: RaisedButton(
+                child: Text('Add Flyer'),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    print ("Pressed");
+                  }
+                }
+              ),
+            ),
           ]
         )
       ),
