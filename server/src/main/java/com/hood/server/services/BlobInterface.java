d92e728b49807ba4c3fbf59b987204666663be30
Shimon Magal <shimon.magal@gmail.com>
2020-07-16 21:19:35 +0300

    adjust some apis to the refactor

--- server/src/main/java/com/hood/server/services/BlobInterface.java
+++ server/src/main/java/com/hood/server/services/BlobInterface.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.hood.server.config.HoodConfig;
+import config.HoodConfig;
 
 public class BlobInterface
 {
